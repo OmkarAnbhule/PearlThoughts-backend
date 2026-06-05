4112b8df480504d2ee57eb9fedf0a166fa0e50ed
OmkarAnbhule <omkaranbhule75@gmail.com>
2026-06-05 10:55:37 +0530

    feat(exception): implement global exception handling with custom error responses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GlobalExceptionFilter } from './common/exceptions';
 import { LoggerModule } from './common/logger';
 import databaseConfig from './config/database.config';
 import { DatabaseModule, databaseConfigModule } from './database/database.module';
@@ -17,6 +19,12 @@ import { DatabaseModule, databaseConfigModule } from './database/database.module
     DatabaseModule.forRoot(),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
